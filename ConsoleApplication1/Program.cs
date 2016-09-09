74f7f6f2b0994b3eb777a9e93a1b8559118a81d0
Lui420 <lewi.lindstrom@gmail.com>
2016-09-09 14:58:32 +0200

    Öving1+2

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,6 +10,61 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            //ovning1
+            Console.WriteLine("Mata i tal 1-4");
+            int tal = int.Parse(Console.ReadLine());
+
+            switch (tal)
+            {
+                case 1:
+                    Console.WriteLine("ett");
+                    break;
+                case 2:
+                    Console.WriteLine("två");
+                    break;
+                case 3:
+                    Console.WriteLine("tre");
+                    break;
+                case 4:
+                    Console.WriteLine("fyra");
+                    break;
+                default:
+                    Console.WriteLine("ogiltlig alternativ");
+                    break;
+            }
+
+
+
+            //ovning2
+            Console.WriteLine("Mata i bokstaven n,s,e,w");
+            string bokstav = Console.ReadLine();
+
+            switch (bokstav)
+            {
+                case "n":
+                    Console.WriteLine("You walk north");
+                    break;
+                case "s":
+                    Console.WriteLine("You travel south");
+                    break;
+                case "e":
+                    Console.WriteLine("You wander east");
+                    break;
+                case "w":
+                    Console.WriteLine("You stroll west");
+                    break;
+                default:
+                    Console.WriteLine("i dont understand");
+                    break;
+
+            }
+
+            //ovning3
+
+            Console.ReadKey();
         }
     }
 }
+         
+        
+    
